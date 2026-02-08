37f0f374c6e707398d529d22feef6db948b76d92
juledahmke <jule.dahmke@tu-dortmund.de>
2026-02-08 03:03:02 +0100

    Update Aufgabe 2.R

--- Aufgabe 2.R
+++ Aufgabe 2.R
@@ -27,8 +27,8 @@ Metrische_Variablen <- function(data, var) {
     Variance = var(x, na.rm = TRUE),
     Minimum = min(x, na.rm = TRUE),
     Maximum = max(x, na.rm = TRUE),
-    Q1 = quantile(x, 0.25, na.rm = TRUE),
-    Q3 = quantile(x, 0.75, na.rm = TRUE),
+    Q25 = as.numeric(quantile(x, 0.25, na.rm = TRUE)),
+    Q75 = as.numeric(quantile(x, 0.75, na.rm = TRUE)),
     Range = max(x, na.rm = TRUE) - min(x, na.rm = TRUE)
   )
   return(result)
@@ -77,13 +77,13 @@ kategoriale_Variablen <- function(x) {
   # Gibt die Ergebnisse in einer Liste zurück.
 }
 
-Ueberlebt <- Titanic_1$Survived
-Klassen <- Titanic_1$Pclass
-Geschlecht <- Titanic_1$Sex
-Hafen <- Titanic_1$Embarked
-Titel <- Titanic_1$Title
-Schiffsseite <- Titanic_1$Side
-Deck <- Titanic_1$Deck
+#Ueberlebt <- Titanic_1$Survived
+#Klassen <- Titanic_1$Pclass
+#Geschlecht <- Titanic_1$Sex
+#Hafen <- Titanic_1$Embarked
+#Titel <- Titanic_1$Title
+#Schiffsseite <- Titanic_1$Side
+#Deck <- Titanic_1$Deck
 # Neue Zuordnung der kategorialen Variablen, für einfachere Nutzung der Funktion.
 
 kategoriale_Variablen(Ueberlebt)
