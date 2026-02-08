e3b4ee8f2a65245918457acc465013af39f69b89
Samuel-ger <samuelkhari@gmail.com>
2026-02-08 13:07:17 +0100

    Update Aufgabe 4.R

--- Aufgabe 4.R
+++ Aufgabe 4.R
@@ -25,4 +25,15 @@ Kontigenztafel(Titanic_1$Survived, Titanic_1$Pclass)
 Kontigenztafel(Titanic_1$Sex, Titanic_1$Pclass)
 Kontigenztafel(Titanic_1$Pclass, Titanic_1$SibSp)
 
+analyze_metric_dichotomous(metric_var = "SibSp", dichotomous_var = "Survived", 
+                           data = Titanic_1)
+## Der Unterschied beim Mittelwert zur Anzahl der Geschwister und Ehefrauen
+## ist zwischen Überlebt "Ja" und "Nein" nicht sehr groß. Die Standardabweichung 
+## ist bei denen die nicht überlebt haben um ca. 0.58 größer.
 
+analyze_metric_dichotomous(metric_var = "Fare", dichotomous_var = "Survived", 
+                           data = Titanic_1)
+## Der Mittelwert des Preises und die Standardabweichung ist bei den 
+## Überlebenden nahezu doppelt so groß.
+
+mosaic_kat(Titanic_1, c("Pclass", "Embarked", "Sex"))
